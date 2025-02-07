add95f56ab1765695faa5fd59d0b24094f1b74c4
Clayton Oliveira <clayton.will08@gmail.com>
2025-02-06 22:10:12 -0300

    Criação Menu

---
+++ TudoFilmes/script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    keyboard: true,
+  }); 
+
+  function carrosselParaHeader(){
+    var swiper = new Swiper(".swiper", {
+      cssMode: true,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      keyboard: true,
+    });  
+  } 
